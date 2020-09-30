c92896fa00299f6b4232a502337711a30dfee83f
oskery <oskar.lyrstrand@gmail.com>
2020-09-30 20:08:47 +0200

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,16 @@
 <template>
   <div id="app">
-    <div class="strap strap-1">
-      <div class="left">
-        <div>
-          <div
-            class="green"
-            :style="{ width: green + '%' }"
-            @click="increaseColor('green')"
-          >
-            <icon-checkmark v-if="green === 100" />
+    <div class="strap" v-for="(c, i) in colors" :key="i">
+      <div :class="c" @click="resetColors"></div>
+      <div>
+        <div :class="c" :style="{ width: c + '%' }" @click="increaseColor(c)">
+          <icon-checkmark v-if="colors[c] === 100" />
+          <div v-else>
+            <span>{{ colors[c] / 10 }}</span> / 10
           </div>
-          <p v-if="green !== 100"><span>{{ green / 10 }}</span> / 10</p>
         </div>
-        <div>
-          <div
-            class="pink"
-            :style="{ width: pink + '%' }"
-            @click="increaseColor('pink')"
-          >
-            <icon-checkmark v-if="pink === 100" />
-          </div>
-          <p v-if="pink !== 100"><span>{{ pink / 10 }}</span> / 10</p>
-        </div>
-        <div>
-          <div
-            class="orange"
-            :style="{ width: orange + '%' }"
-            @click="increaseColor('orange')"
-          >
-            <icon-checkmark v-if="orange === 100" />
-          </div>
-          <p v-if="orange !== 100"><span>{{ orange / 10 }}</span> / 10</p>
-        </div>
-        <div>
-          <div
-            class="red"
-            :style="{ width: red + '%' }"
-            @click="increaseColor('red')"
-          >
-            <icon-checkmark v-if="red === 100" />
-          </div>
-          <p v-if="red !== 100"><span>{{ red / 10 }}</span> / 10</p>
-        </div>
+        <div></div>
       </div>
-      <div class="right green" @click="resetColors"></div>
     </div>
   </div>
 </template>
@@ -51,8 +18,10 @@
 <script>
 export default {
   name: 'App',
+
   data() {
     return {
+      colors: ['green', 'pink', 'orange', 'red'],
       lastCleared: 'green',
       green: 10,
       pink: 20,
@@ -86,52 +55,50 @@ body {
   align-items: center;
   justify-content: center;
   height: 200vh;
-  background: black; // #e9e9e9;
-  font-family: 'SquadaOne';
+  background: white; // #e9e9e9;
+}
+.strap {
+  display: flex;
+  border-radius: 10px;
+  width: 100vw;
+  height: calc(320px - 120px);
+  overflow: hidden;
+  margin-bottom: 20px;
+  div:first-child {
+    background: red;
+    width: 25%;
+  }
 }
 
 .strap {
-  display: flex;
-  background: white;
-  border-radius: 10px;
-  width: 562px;
-  height: calc(320px - 120px);
-  overflow: hidden;
-  border: 3px solid white;
-  margin-bottom: 20px;
-}
-
-.strap-1 {
-  .left > div,
-  .right {
-    cursor: pointer;
-  }
-  .left > div:hover,
-  .right:hover {
-    filter: brightness(1.2);
-  }
-  .left {
+  div:nth-child(2) {
     display: flex;
     flex-direction: column;
-    width: 75%;
-    margin-right: 3px;
+    margin-left: 3px;
+    flex: 1;
     > div {
       flex: 1;
       display: flex;
       align-items: center;
-      > div {
-        display: flex;
-        align-items:center;
-        justify-content: center;
-        color: rgba(black, .7);
+      background: white;
+      > div:first-child {
+        color: white;
         height: 100%;
         transition: 1s width;
       }
+      > div:nth-child(2) {
+        opacity: 0.3;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+      }
       p {
         margin: 0 10px;
-        white-space: nowrap;        span {
+        white-space: nowrap;
+        span {
           font-size: 2rem;
-          font-weight:bold;
+          font-weight: bold;
         }
       }
     }
@@ -139,12 +106,6 @@ body {
       margin-bottom: 3px;
     }
   }
-  .right {
-    flex: 1;
-  }
-}
-
-.strap-2 {
 }
 
 .red {
@@ -158,19 +119,5 @@ body {
 }
 .green {
   background: #00e761;
-}
-button {
-  padding: 10px 20px;
-  font-size: 1.4rem;
-  cursor: pointer;
-  margin-right: 20px;
-}
-
-
-@font-face {
-  font-family: 'SquadaOne';
-  src: url('~@/assets/SquadaOne-Regular.ttf');
-  font-weight: normal;
-  font-style: normal;
 }
 </style>
---
+++ src/views/Strap.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="strap strap-1">
+    <div class="green" @click="resetColors"></div>
+    <div>
+      <div>
+        <div
+          class="green"
+          :style="{ width: green + '%' }"
+          @click="increaseColor('green')"
+        >
+          <icon-checkmark v-if="green === 100" />
+          <div v-else>
+            <span>{{ green / 10 }}</span> / 10
+          </div>
+        </div>
+        <div class="green"></div>
+      </div>
+      <div>
+        <div
+          class="pink"
+          :style="{ width: pink + '%' }"
+          @click="increaseColor('pink')"
+        >
+          <icon-checkmark v-if="pink === 100" />
+          <div v-else>
+            <span>{{ pink / 10 }}</span> / 10
+          </div>
+        </div>
+        <div class="pink">
+          <p v-if="pink !== 100"></p>
+        </div>
+      </div>
+      <div>
+        <div
+          class="orange"
+          :style="{ width: orange + '%' }"
+          @click="increaseColor('orange')"
+        >
+          <icon-checkmark v-if="orange === 100" />
+          <p v-else>
+            <span>{{ orange / 10 }}</span> / 10
+          </p>
+        </div>
+        <div class="orange"></div>
+      </div>
+      <div>
+        <div
+          class="red"
+          :style="{ width: red + '%' }"
+          @click="increaseColor('red')"
+        >
+          <icon-checkmark v-if="red === 100" />
+          <p v-else>
+            <span>{{ red / 10 }}</span> / 10
+          </p>
+        </div>
+        <div class="red"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lastCleared: 'green',
+      green: 10,
+      pink: 20,
+      orange: 60,
+      red: 100
+    }
+  },
+  computed: {
+    style(x) {
+      return { width: this[x] + '%' }
+    }
+  },
+  components: {
+    IconCheckmark: () => import('@/components/icons/IconCheckmark.vue')
+  },
+  methods: {
+    increaseColor(c) {
+      this[c] = Math.min(this[c] + 20, 100)
+    },
+    resetColors() {
+      this.lastCleared = 'green'
+      ;(this.green = 10), (this.pink = 20), (this.orange = 60), (this.red = 100)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.strap {
+  display: flex;
+  border-radius: 10px;
+  width: 100vw;
+  height: calc(320px - 120px);
+  overflow: hidden;
+  margin-bottom: 20px;
+}
+
+.strap-1 {
+  div:nth-child(2) > div,
+  .left {
+    cursor: pointer;
+  }
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+    width: 75%;
+    margin-right: 3px;
+    > div {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      background: white;
+      > div:first-child {
+        color: white;
+        height: 100%;
+        transition: 1s width;
+      }
+      > div:nth-child(2) {
+        opacity: .3;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+      }
+      p {
+        margin: 0 10px;
+        white-space: nowrap;
+        span {
+          font-size: 2rem;
+          font-weight: bold;
+        }
+      }
+    }
+    > div:not(:last-child) {
+      margin-bottom: 3px;
+    }
+  }
+   div:nth-child(1) {
+    flex: 1;
+  }
+}
+
+.strap-2 {
+}
+
+.red {
+  background: #e0424d;
+}
+.orange {
+  background: #e8733b;
+}
+.pink {
+  background: #de1d71;
+}
+.green {
+  background: #00e761;
+}
+
+.green-opacity {
+  background: rgba(#00e761, 0.2);
+}
+
+.orange-opacity {
+  background: rgba(#e8733b, 0.2);
+}
+
+.pink-opacity {
+  background: rgba(#de1d71, 0.2);
+}
+
+.green-opacity {
+  background: rgba(#00e761, 0.2);
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  cursor: pointer;
+  margin-right: 20px;
+}
+</style>
